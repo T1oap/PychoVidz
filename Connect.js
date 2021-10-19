24d5f3e5bc6c89cb64f9b6583bbce48c0f711ff0
T1oap <92763970+T1oap@users.noreply.github.com>
2021-10-19 18:38:42 -0400

    Create Connect.js

---
+++ Connect.js
@@ -0,0 +1,17 @@
+function myfunction();{
+  var CubeAreaLine = tabsize(100({1}));{
+  var aria = popup(20);{
+     }
+  }
+}
+ const information = true(12, 34)
+ const informationsCalls = true(12)
+ 
+ 
+ 
+   set.new.project(name("new Script"))
+   set.new.page(name("reflesh page?")) 
+
+   
+   
+    
